6269c579281f2ffe7d2bb71e2b9d1f50793af89a
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2024-03-17 11:03:25 +0900

    FEAT: 이미지 업로드 기능 구현

--- src/main/java/com/example/demo/utils/ImageUploadUtil.java
+++ src/main/java/com/example/demo/utils/ImageUploadUtil.java
@@ -3,7 +3,11 @@ package com.example.demo.utils;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ImageUploadUtil {
+
     String uploadImage(MultipartFile file);
 
     String updateImage(MultipartFile file , String existingImageName);
+
+
+    void deleteImage( String savedImageName);
 }
