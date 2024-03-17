d0046ab89172870d15ddde0147700b547c2bf6e8
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2024-03-17 10:33:16 +0900

    FEAT: 카테고리 컨트롤러 추가

--- src/main/java/com/example/demo/web/controller/view/category/CategoryViewController.java
+++ src/main/java/com/example/demo/web/controller/view/category/CategoryViewController.java
@@ -3,6 +3,7 @@ package com.example.demo.web.controller.view.category;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,8 +13,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/manage")
 public class CategoryViewController {
 
-    @GetMapping("/category-group")
-    public String registerCategoryGroup(){
-        return "";
+    @GetMapping("/register/category-group")
+    public String registerCategoryGroup(Model model){
+        model.addAttribute("selectFlag", "registerCategoryGroup");
+        return "manage/category/categorygroup-register";
+    }
+
+    @GetMapping("/update/category-group")
+    public String updateCategoryGroup(Model model){
+        model.addAttribute("selectFlag", "updateCategoryGroup");
+        return "manage/category/categorygroup-update";
+    }
+
+    @GetMapping("/register/category")
+    public String registerCategory(Model model){
+        model.addAttribute("selectFlag", "registerCategory");
+        return "manage/category/category-register";
+    }
+
+    @GetMapping("/update/category")
+    public String updateCategory(Model model){
+        model.addAttribute("selectFlag", "updateCategory");
+        return "manage/category/category-update";
     }
 }
