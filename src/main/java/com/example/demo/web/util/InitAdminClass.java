afad07b3fcde3fd219bf5f942f2a4f85d360f5ec
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2024-05-09 12:08:57 +0900

    FEAT : 관리자 아이디 데이터

--- src/main/java/com/example/demo/web/util/InitAdminClass.java
+++ src/main/java/com/example/demo/web/util/InitAdminClass.java
@@ -15,7 +15,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-@Profile("local")
 @Component
 @RequiredArgsConstructor
 public class InitAdminClass {
